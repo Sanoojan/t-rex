88ba1ac4f7d5008638402d5dcd7a297a6a9d04e3
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2023-02-05 17:19:44 +0400

    [add] barlowtin datatransform + refactor

--- datacode/classifier_data.py
+++ datacode/classifier_data.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 import pandas as pd
 import scipy.io
-from PIL import Image
+from PIL import Image, ImageOps, ImageFilter
 
 import torch
 import torchvision
@@ -203,17 +203,38 @@ class FoodXDataset(torch.utils.data.Dataset):
 
 ## ========================= Transforms ========================================
 
+IMG_SIZE = 299
+
+
+class InferTransforms:
+    def __init__(self, infer=False):
+        self.img_size =  IMG_SIZE
+        self.transform = transforms.Compose([
+            transforms.Resize((self.img_size, self.img_size)),
+            transforms.ToTensor(),
+            transforms.Normalize( mean=(0.485, 0.456, 0.406),
+                                  std=(0.229, 0.224, 0.225)),
+        ])
+    def __call__(self, x):
+        y = self.transform(x)
+        return y
+    def get_composition(self):
+        return str(self.transform)
+
+
 class ClassifyTransforms:
     def __init__(self, infer=False):
-        self.img_size =  224
+        self.img_size =  IMG_SIZE
         train_transform = transforms.Compose([
-            transforms.RandomResizedCrop(self.img_size, scale=(0.6, 1.0),
-                        interpolation=transforms.InterpolationMode.BICUBIC),
+            transforms.Resize((self.img_size, self.img_size)),
+            # transforms.RandomResizedCrop(self.img_size, scale=(0.6, 1.0),
+            #             interpolation=transforms.InterpolationMode.BICUBIC),
             # transforms.RandomAffine(degrees=(-180, 180), translate=(0.2, 0.2),
             #             interpolation=transforms.InterpolationMode.BICUBIC),
-            transforms.AutoAugment(),
+            transforms.RandAugment(),
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomVerticalFlip(p=0.5),
+            # transforms.AugMix(),
             transforms.ToTensor(),
             transforms.Normalize( mean=(0.485, 0.456, 0.406),
                                   std=(0.229, 0.224, 0.225)),
@@ -233,31 +254,68 @@ class ClassifyTransforms:
         return y
 
     def get_composition(self):
-        return self.transform
+        return str(self.transform)
 
 
-class DinoTransforms:
+class GaussianBlur(object):
+    def __init__(self, p):
+        self.p = p
+
+    def __call__(self, img):
+        if random.random() < self.p:
+            sigma = random.random() * 1.9 + 0.1
+            return img.filter(ImageFilter.GaussianBlur(sigma))
+        else:
+            return img
+
+
+class Solarization(object):
+    def __init__(self, p):
+        self.p = p
+
+    def __call__(self, img):
+        if random.random() < self.p:
+            return ImageOps.solarize(img)
+        else:
+            return img
+
+
+class BarlowTransforms:
     def __init__(self):
-        self.img_size =  224
+        self.img_size =  IMG_SIZE
         self.transform = transforms.Compose([
-            transforms.RandomResizedCrop(self.img_size, scale=(0.7, 1.0),
-                        interpolation=transforms.InterpolationMode.BICUBIC),
-            transforms.RandomAffine(degrees=(-180, 180), translate=(0.2, 0.2),
-                        interpolation=transforms.InterpolationMode.BICUBIC),
-            transforms.ColorJitter(brightness=0.5),
+            transforms.Resize((self.img_size, self.img_size), 
+                interpolation=transforms.InterpolationMode.BICUBIC),
+            # transforms.RandomResizedCrop(self.img_size, 
+            #   interpolation=Image.BICUBIC),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomApply(
+                [transforms.ColorJitter(brightness=0.4, contrast=0.4,
+                                        saturation=0.2, hue=0.1)],  p=0.8 ),
+            transforms.RandomGrayscale(p=0.2),
+            GaussianBlur(p=1.0),
+            Solarization(p=0.0),
+            transforms.RandAugment(),
             transforms.ToTensor(),
-
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
         ])
         self.transform_prime = transforms.Compose([
-            transforms.RandomResizedCrop(self.img_size, scale=(0.6, 1.0),
-                        interpolation=transforms.InterpolationMode.NEAREST),
-            transforms.RandomPerspective(distortion_scale=0.6, p=0.5,
-                        interpolation=transforms.InterpolationMode.NEAREST),
-            transforms.RandomAutocontrast(p=0.7),
+            transforms.Resize((self.img_size, self.img_size), 
+                interpolation=transforms.InterpolationMode.BICUBIC),
+            # transforms.RandomResizedCrop(self.img_size, 
+            #   interpolation=Image.BICUBIC),
             transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomVerticalFlip(p=0.5),
+            transforms.RandomApply(
+                [transforms.ColorJitter(brightness=0.4, contrast=0.4,
+                                        saturation=0.2, hue=0.1)], p=0.8 ),
+            transforms.RandomGrayscale(p=0.2),
+            GaussianBlur(p=0.1),
+            Solarization(p=0.2),
+            transforms.RandAugment(),
             transforms.ToTensor(),
-
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
         ])
 
     def __call__(self, x):
@@ -265,6 +323,8 @@ class DinoTransforms:
         y2 = self.transform_prime(x)
         return y1, y2
 
+    def get_composition(self):
+        return str(self.transform) + str(self.transform_prime)
 
 ## ===================== Dataloaders Func ======================================
 
@@ -275,64 +335,78 @@ class SimplifiedLoader():
         self.foodxdata_path =  "/apps/local/shared/CV703/datasets/FoodX/food_dataset/"
         self.carsdata_path =  "/apps/local/shared/CV703/datasets/stanford_cars/"
         self.set_name = set_name
-
+        self.data_info = {}
 
 
     def get_data_loader(self, type_, batch_size=64, workers=2, augument= "DEFAULT"):
-        dataset = self._fetch_dataset(self.set_name, type_, augument)
 
-        if augument == "DEFAULT" or type_ != "train":
-            loader, data_info = self._default_loader_impl(dataset,
-                                batch_size=batch_size, workers=workers,
-                                type_= type_)
-        elif augument == "AUGMIX":
+        self.data_info["type"] = type_
+        transform = self._fetch_transforms(type_, augument)
+        dataset = self._fetch_dataset(self.set_name, type_, transform)
+
+
+        loader = self._default_loader_impl(dataset,
+                            batch_size=batch_size, workers=workers,
+                            type_= type_)
+
+        if augument == "AUGMIX": # override loader
             print("Loading Augmix based loader from timm !.!.!")
-            loader, data_info = self._augmix_loader_impl(dataset,
+            loader = self._augmix_loader_impl(dataset,
                                 batch_size=batch_size, workers=workers,
                                 splits=3, type_=type_)
 
-        return loader, data_info
+        return loader, self.data_info.copy()
 
+    def _fetch_transforms(self, type_, augument):
 
-    def _fetch_dataset(self, set_name, type_, augument):
-
-        infer_flag = False
-        if type_ in ['valid', 'test', 'infer']:
-            infer_flag = True
-
-        if augument == "DEFAULT" or infer_flag:
-            data_transform = ClassifyTransforms(infer_flag)
+        if (type_ in ["valid", "test", "infer"]) or (augument in ["INFER", None]):
+            data_transform = InferTransforms()
+        elif augument == "DEFAULT":
+            data_transform = ClassifyTransforms()
         elif augument == "AUGMIX":
-            data_transform = None
+            data_transform = None # will be set by _augmix_loader_impl
+        elif augument == "BARLOW":
+            data_transform = BarlowTransforms()
         else:
             raise ValueError("Unknown augument specified")
 
 
+        self.data_info["transform"] = data_transform.get_composition() if data_transform else None
+
+        return data_transform
+
+
+    def _fetch_dataset(self, set_name, type_, transform):
+        infer_flag = False
+        if type_ in ['valid', 'test', 'infer']:
+            infer_flag = True
+
         if set_name == "air":
             dataset = FGVCAircraft(data_dir=self.aircraftsdata_path,
-                                    mode=type_, transform=data_transform)
+                                    mode=type_, transform=transform)
         elif set_name == "car":
            dataset = StanfordCars(data_dir=self.carsdata_path,
-                                    mode=type_, transform=data_transform)
-
+                                    mode=type_, transform=transform)
         elif set_name == "food":
            dataset = FoodXDataset(data_dir=self.foodxdata_path,
-                                    mode=type_, transform=data_transform)
-
+                                    mode=type_, transform=transform)
         elif set_name == "air+car":
-            air_dataset = FGVCAircraft(data_dir=self.aircraftsdata_path, mode=type_,
-                            transform=data_transform)
-            car_dataset = StanfordCars(data_dir=self.carsdata_path, mode=type_,
+            air_dataset = FGVCAircraft(data_dir=self.aircraftsdata_path,
+                            mode=type_, transform=transform)
+            car_dataset = StanfordCars(data_dir=self.carsdata_path,
+                            mode=type_,
                             offset_class=len(air_dataset.class_to_idx),
-                            transform=data_transform )
-
+                            transform=transform)
             dataset = ConcatDataset([air_dataset, car_dataset])
             dataset.class_to_idx = dict(list(air_dataset.class_to_idx.items()) +  \
                     list(car_dataset.class_to_idx.items())) #union operation
-            dataset.transform = data_transform
+            dataset.transform = transform
         else:
             raise ValueError("Unknown Dataset indicator set")
 
+        self.data_info.update({ "Classes": dataset.class_to_idx ,
+                    "ClassesSize": len(dataset.class_to_idx) ,
+                    "DatasetSize": dataset.__len__() })
         return dataset
 
 
@@ -341,34 +415,21 @@ class SimplifiedLoader():
         if type_ in ['valid', 'test', 'infer']:
             shuffle_flag = False
 
-        data_info = {"type": type_,
-                    "Classes": dataset.class_to_idx ,
-                    "ClassesSize": len(dataset.class_to_idx) ,
-                    "DatasetSize": dataset.__len__(),
-                    "Transforms": "DEFAULT"#str(dataset.transform)
-                    }
-
         loader = torch.utils.data.DataLoader( dataset,
             batch_size=batch_size, num_workers=workers, shuffle=shuffle_flag,
             pin_memory=True)
 
-        return loader, data_info
+        return loader
 
 
     def _augmix_loader_impl(self, dataset, batch_size=64, workers=2,
                             splits = 3, type_ = "train"):
         ## TODO: figure out and Fix issue with concat dataset
-        """
+        """ timm library based usage
         Reference:[1]https://github.com/rwightman/pytorch-image-models/blob/main/timm/data/loader.py#L189
                 [*2] https://github.com/rwightman/pytorch-image-models/blob/d5aa17e41572ececee0f7829ec1640384532c5d2/timm/data/auto_augment.py#L951
         """
         assert type_ == "train",  "AUGMIX can be invoked only for training"
-
-        data_info = {"type": type_,
-                    "Classes": dataset.class_to_idx ,
-                    "ClassesSize": len(dataset.class_to_idx) ,
-                    "DatasetSize": dataset.__len__(),
-                    "Transforms": "AUGMIX" }
 
         dataset = timm.data.AugMixDataset(dataset, num_splits=splits)
         loader = timm.data.create_loader(dataset,
@@ -391,11 +452,11 @@ class SimplifiedLoader():
                     pin_memory=True,           )
 
 
-        return loader, data_info
+        return loader
 
 
 ##------------------------------------------------------------------------------
-## Naive Individual Loader TODO: Remove
+## Naive Individual Loader TODO: Remove when refactoring
 
 def getCifar100Loader(folder, batch_size, workers=2, type_ = 'train'):
 
@@ -514,7 +575,6 @@ def getFoodxLoader(folder, batch_size, workers=2, type_ = 'train'):
 
 
 
-
 ##========================= DatasetChecking ====================================
 
 import matplotlib.pyplot as plt
@@ -545,8 +605,8 @@ if __name__ == "__main__":
 
     # dataloader,_ = getCarsLoader( carsdata_path, batch_size=2, type_="valid")
 
-    dataloader, _ = SimplifiedLoader("air").getDataLoader(type_= "valid",
-                    batch_size=2, workers=2, augument= "AUGMIX")
+    dataloader, _ = SimplifiedLoader("car").get_data_loader(type_= "train",
+                    batch_size=2, workers=2, augument= "BARLOW")
 
 
     ## -------------------- Plotting Loop---------------------------------------
@@ -561,12 +621,13 @@ if __name__ == "__main__":
 
     iter_dataloader = iter(dataloader)
     for i in range(count):
-        img, tgt =  next(iter_dataloader) ## ridx will be ignored
-        print(img.min(), img.max())
-        img_ = [ img[i].squeeze() for i in range(img.shape[0]) ]
-        tgt_ = [ tgt[i].squeeze() for i in range(tgt.shape[0]) ]
-        imgs.extend(img_)
-        tgts.extend(tgt_)
+        img_ret, tgt =  next(iter_dataloader) ## ridx will be ignored
+        for img in img_ret: # for handling image augumentation
+            print(img.min(), img.max())
+            img_ = [ img[i].squeeze() for i in range(img.shape[0]) ]
+            tgt_ = [ tgt[i].squeeze() for i in range(tgt.shape[0]) ]
+            imgs.extend(img_)
+            tgts.extend(tgt_)
 
     show_loaded_imgs(imgs)
 
