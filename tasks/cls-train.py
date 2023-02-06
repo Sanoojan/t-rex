28248e97789775e6e57946d7cff832753fe70f03
sanoojan <sanoojan24@gmail.com>
2023-02-06 22:18:34 +0400

    transformer

--- tasks/cls-train.py
+++ tasks/cls-train.py
@@ -1,28 +1,33 @@
+
+
+
 """ Classifier Network trainig
 """
 
+import os, sys, time
 import argparse
 import json
-import math
-import os
-import random
-import signal
-import subprocess
-import sys
-import time
+import random, math
+import signal, subprocess
 
 from tqdm.autonotebook import tqdm
 import PIL.Image
-from torch import nn, optim
+
 import torch
+from torch import nn, optim
+import timm
+import timm.loss
 
 sys.path.append(os.getcwd())
 import utilities.runUtils as rutl
 import utilities.logUtils as lutl
 from utilities.metricUtils import MultiClassMetrics
-from algorithms.resnet import ClassifierNet
-import datacode.classifier_data as ClsData
-from algorithms.visiontransformer import vit_small_patch16_224
+from algorithms.visiontransformer import vit_small_patch16_224, vit_base_patch16_224, vit_base_patch16_224_in21k
+# from algorithms.resnet import ClassifierNet
+from algorithms.convnext import ClassifierNet
+import algorithms.clip as clip
+
+from datacode.classifier_data import SimplifiedLoader
 
 
 print(f"Pytorch version: {torch.__version__}")
@@ -30,49 +35,52 @@ print(f"cuda version: {torch.version.cuda}")
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print("Device Used:", device)
 
-###============================= Configure and Setup ===========================
 
-# #for ResNet
-# cfg = rutl.ObjDict(
-# dataset = "air", # "air+car", "food", "car"
-# balance_class = False, #to be implemented
+##============================= Configure and Setup ============================
 
-# epochs= 1000,
-# batch_size= 64,
-# workers= 4,
-# learning_rate= 1e-4,
-# weight_decay= 1e-6,
-
-# feature_extract = "resnet50", # "resnet34/50/101"
-# featx_pretrain = "DEFAULT",  # path-to-weights or None or DEFAULT-->imagenet
-# featx_dropout = 0.0,
-# classifier = [1024,], #First & Last MLPs will be set in code based on class out of dataset and FeatureExtractor
-# clsfy_dropout = 0.5,
-
-# checkpoint_dir= "hypotheses/res50-air/",
-# restart_training=False
-# )
-
-#for ViT
 cfg = rutl.ObjDict(
-dataset = "food", # "air+car", "food", ## "car"
+dataset = "air", # "air+car", "food", "car"
 balance_class = False, #to be implemented
 
 epochs= 1000,
-batch_size= 100,
+batch_size= 80,
 workers= 4,
-learning_rate= 5e-5,
-weight_decay= 0.001,
+learning_rate= 1e-4,
+weight_decay= 1e-6,
+augument= "DEFAULT", #DEFAULT or AUGMIX
 
-feature_extract = "resnet50", # "resnet34/50/101"
+feature_extract = "inceptionV4", # "resnet34/50/101"  convnext-small
 featx_pretrain = "DEFAULT",  # path-to-weights or None or DEFAULT-->imagenet
 featx_dropout = 0.0,
 classifier = [1024,], #First & Last MLPs will be set in code based on class out of dataset and FeatureExtractor
 clsfy_dropout = 0.5,
 
-checkpoint_dir= "hypotheses/vit_small-food/",
+checkpoint_dir= "hypotheses/inceptionv4-air/",
 restart_training=True
 )
+
+# dataset_to_train="air"  # "air+car", "food", ## "car"
+# cfg = rutl.ObjDict(
+# dataset = dataset_to_train,
+# checkpoint_dir= "hypotheses/base_21k"+dataset_to_train+"/",
+# balance_class = False, #to be implemented
+
+# epochs= 200,
+# batch_size= 80,
+# workers= 4,
+# learning_rate= 5e-5,
+# weight_decay= 0.0001,
+# augument= "DEFAULT", #DEFAULT or AUGMIX,
+
+# feature_extract = "vitbase", # "resnet34/50/101"
+# featx_pretrain = "DEFAULT",  # path-to-weights or None or DEFAULT-->imagenet
+# featx_dropout = 0.0,
+# classifier = [1024,], #First & Last MLPs will be set in code based on class out of dataset and FeatureExtractor
+# clsfy_dropout = 0.5,
+
+
+# restart_training=True
+# )
 
 ### -----
 parser = argparse.ArgumentParser(description='Classification task')
@@ -93,49 +101,29 @@ cfg.gWeightPath = cfg.checkpoint_dir + '/weights/'
 
 
 def getDatasetSelection():
-    aircraftsdata_path = "/nfs/users/ext_sanoojan.baliah/Sanoojan/data/fgvc-aircraft-2013b/"
-    foodxdata_path =  "/nfs/users/ext_sanoojan.baliah/Sanoojan/data/FoodX/food_dataset/"
-    carsdata_path =  "/nfs/users/ext_sanoojan.baliah/Sanoojan/data/stanford_cars/"
 
-    if cfg.dataset == "air":
-        trainloader, train_info = ClsData.getAircraftsLoader( aircraftsdata_path,
-                            batch_size=cfg.batch_size, workers =cfg.workers,
-                            type_="train" )
-        validloader, valid_info = ClsData.getAircraftsLoader( aircraftsdata_path,
-                            batch_size=cfg.batch_size, workers =cfg.workers,
-                            type_="valid" )
-    elif cfg.dataset == "car":
-        trainloader, train_info = ClsData.getCarsLoader( carsdata_path,
-                            batch_size=cfg.batch_size, workers =cfg.workers,
-                            type_="train" )
-        validloader, valid_info = ClsData.getCarsLoader(carsdata_path,
-                            batch_size=cfg.batch_size, workers =cfg.workers,
-                            type_="valid" )
+    loaderObj = SimplifiedLoader("air")
+    trainloader, train_info = loaderObj.getDataLoader(type_= "train", 
+                    batch_size=cfg.batch_size, workers=cfg.workers, 
+                    augument= cfg.augument)
 
-    elif cfg.dataset == "food":
-        trainloader, train_info = ClsData.getFoodxLoader( foodxdata_path,
-                            batch_size=cfg.batch_size, workers =cfg.workers,
-                            type_="train" )
-        validloader, valid_info = ClsData.getFoodxLoader( foodxdata_path,
-                            batch_size=cfg.batch_size, workers =cfg.workers,
-                            type_="valid" )
-
-    elif cfg.dataset == "air+car":
-        trainloader, train_info = ClsData.getAircraftsAndCarsLoader(
-                            [aircraftsdata_path, carsdata_path],
-                            batch_size=cfg.batch_size, workers =cfg.workers,
-                            type_="train" )
-        validloader, valid_info = ClsData.getAircraftsAndCarsLoader(
-                            [aircraftsdata_path, carsdata_path],
-                            batch_size=cfg.batch_size, workers =cfg.workers,
-                            type_="valid" )
-    else:
-        raise ValueError("Unknown Dataset indicator set")
+    validloader, valid_info = loaderObj.getDataLoader(type_= "valid", 
+                    batch_size=cfg.batch_size, workers=cfg.workers,
+                    augument= "DEFAULT")
 
     lutl.LOG2DICTXT(["Train-",train_info], cfg.gLogPath +'/misc.txt')
     lutl.LOG2DICTXT(["Valid-", valid_info], cfg.gLogPath +'/misc.txt')
 
     return trainloader, validloader, len(train_info["Classes"])
+
+
+def getLossSelection():
+    train_loss = valid_loss = nn.CrossEntropyLoss()
+    if cfg.augument == "AUGMIX":
+        train_loss = timm.loss.JsdCrossEntropy(num_splits=3)
+
+    return train_loss, valid_loss
+
 
 
 def simple_main():
@@ -159,15 +147,29 @@ def simple_main():
     cfg.classifier.append(class_size) #Adding last layer of MLP
 
     ### MODEL, OPTIM
-
-    #Resnet model
     # model = ClassifierNet(cfg).cuda(gpu)
-    # device=
 
-    model=vit_small_patch16_224(pretrained=True)
-    model.head = nn.Linear(384, class_size)
+    #load inceptionV4 from timm
+    model = timm.create_model('inception_v4', pretrained=True)
+
+  
+
+    # model=vit_base_patch16_224_in21k(pretrained=True)
+    # model.head = nn.Linear(768, class_size)
+
+
+    # clipname = 'ViT-B/16'
+    # mod, preprocess = clip.load(clipname)
+    
+    # mod = mod.visual.float()
+    # lin=torch.nn.Linear(768, class_size)
+    # model=nn.Sequential(mod, lin)
+
+
     model=model.to(device)
-    lossfn = nn.CrossEntropyLoss()
+
+
+    lossfn, v_lossfn = getLossSelection()
     optimizer = optim.AdamW(model.parameters(), lr=cfg.learning_rate,
                         weight_decay=cfg.weight_decay)
     lutl.LOG2TXT(f"Parameters:{rutl.count_train_param(model)}", cfg.gLogPath +'/model-info.txt')
@@ -185,7 +187,6 @@ def simple_main():
 
 
     ### MODEL TRAINING
-    
     start_time = time.time()
     best_acc = 0 ; best_loss = float('inf')
     trainMetric = MultiClassMetrics(cfg.gLogPath)
@@ -196,15 +197,14 @@ def simple_main():
 
         ## ---- Training Routine ----
         model.train()
-        
         for img, tgt in tqdm(trainloader):
             img = img.to(device, non_blocking=True)
             tgt = tgt.to(device, non_blocking=True)
             optimizer.zero_grad()
             ## with mixed precision
-            # with torch.cuda.amp.autocast():
-            pred = model.forward(img)
-            loss = lossfn(pred, tgt)
+            with torch.cuda.amp.autocast():
+                pred = model.forward(img)
+                loss = lossfn(pred, tgt)
             ## END with
             # scaler.scale(loss).backward()
             # scaler.step(optimizer)
@@ -227,7 +227,7 @@ def simple_main():
                 tgt = tgt.to(device, non_blocking=True)
                 # with torch.cuda.amp.autocast():
                 pred = model.forward(img)
-                loss = lossfn(pred, tgt)
+                loss = v_lossfn(pred, tgt)
                 ## END with
                 validMetric.add_entry(torch.argmax(pred, dim=1), tgt, loss)
 
@@ -258,4 +258,4 @@ def simple_main():
         validMetric.reset(best_flag)
 
 if __name__ == '__main__':
-    simple_main()
+    simple_main()    
